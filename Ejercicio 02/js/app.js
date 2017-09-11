c4f3c5f626f991d8cb1285e01959edf5f584c53a
LUIS-PC\Luis <schizophrenia.androi@gmail.com>
2017-09-11 13:23:07 -0500

    funciones en botones y objetos scope

--- Ejercicio 02/js/app.js
+++ Ejercicio 02/js/app.js
@@ -6,8 +6,23 @@ var app = angular.module('universidadApp', [ ]);
 
 app.controller('profesorCtrl', function ($scope) {
 
-	//this.profesor = profesorData; /*delcaramos una propiedad profesor que se iguala a un objeto, propiedad del controlador*/
+	/*this.profesor = profesorData; /*delcaramos una propiedad profesor que se iguala a un objeto, propiedad del controlador*/
 	$scope.profesor = profesorData;
+	$scope.editando = {};
+
+	$scope.EditarProfesor = function() {
+
+		angular.copy( $scope.profesor , $scope.editando);   /*copiamos de un objeto a otro*/
+	}
+
+	$scope.GuardarCambios = function() {
+
+		angular.copy( $scope.editando, $scope.profesor);   /*copiamos de un objeto a otro*/
+	}
+
+	$scope.CancelarCambios = function() {
+		$scope.editando = {};
+	}
 })
 
 var profesorData = {
@@ -16,4 +31,3 @@ var profesorData = {
 	edad: 47,
 	foto: "img/juancarlos.jpg"
 }
-
